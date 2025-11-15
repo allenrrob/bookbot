1be06bfc8c6063b81a9af754add231a07c6b522b
allenrrob <allenrrob@gmail.com>
2025-11-15 15:47:20 -0800

    added character_count to stats.py & altered main.py to run it

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from stats import count_words
+from stats import character_count
 
 def main():
-     count_words("books/frankenstein.txt")
+     character_count("books/frankenstein.txt")
+     
      
      
 
--- stats.py
+++ stats.py
@@ -9,3 +9,16 @@ def count_words(filepath):
      word_count = len(word_list)
           
      print(f"Found {word_count} total words")
+
+def character_count(filepath):
+    character_dict = {}
+    book_as_string = get_book_text(filepath)
+    for char in book_as_string:
+        lower_char = char.lower()
+        if lower_char not in character_dict:
+            character_dict[lower_char]=1
+        else:
+            character_dict[lower_char] += 1
+
+
+    print(character_dict)
